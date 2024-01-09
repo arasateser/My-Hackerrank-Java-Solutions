a22d639e61b17a144686852ea0fa5f5f94fe4823
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-01-09 17:12:46 +0300

    Update Java Subarray.java

--- Java Subarray.java
+++ Java Subarray.java
@@ -1,31 +1,36 @@
 import java.io.*;
 import java.util.*;
 
-public class subarray {
+public class Solution {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        int[] arr = { 1, -2, 4, -5, 1 };
-
-        System.out.println(Arrays.toString(arr));
-        System.out.println();
-
-        // int limit = 0;
-        // int k = 0;
-        for (int z = 0; z < arr.length; z++) {
-            int limit = 0;
-            // int ilk = 0;
-
-            for (int i = 0; i < arr.length; i++) {
-                int k = 0;
-
-                for (int j = 0; j < arr.length - limit; j++) {
-                    System.out.print(arr[j] + " ");
-
-                }
-                limit++;
-                System.out.println("\n");
-            }
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
         }
+
+        int neg_sub = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i; j < arr.length; j++) {
+                int curr_sub = 0;
+
+                for (int k = i; k <= j; k++) {
+                    curr_sub += arr[k];
+                }
+
+                if (curr_sub < 0) {
+                    neg_sub++;
+                }
+
+            }
+
+        }
+        System.out.println(neg_sub);
     }
 }
