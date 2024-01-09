10435301d4e8f896bf6758ef033a94741780c734
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-01-09 15:13:41 +0300

    Update Java Subarray.java

--- Java Subarray.java
+++ Java Subarray.java
@@ -1,29 +1,23 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class subarray {
 
     public static void main(String[] args) {
-        
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-        int[] arr = new int[n];
-        
-        //read elements and store in array
-        for(int i=0; i<n; i++){
-            arr[i] = scn.nextInt();
-        }
-        
+
+        int[] arr = { 1, -2, 4, -5, 1 };
+
         System.out.println(Arrays.toString(arr));
-        
+        System.out.println();
+
         int limit = 0;
-        //int k = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            int k = 0;    
-            for (int j = 0; j < arr.length - limit ; j++) {
+        // int k = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int k = 0;
+            for (int j = 0; j < arr.length - limit; j++) {
                 System.out.print(arr[j] + " ");
-                               
+
             }
             limit++;
             System.out.println("\n");
