f7a6a2938b248ee86e8583a38cdf8fc75ea7017c
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2024-01-09 15:21:29 +0300

    Update Java Subarray.java

--- Java Subarray.java
+++ Java Subarray.java
@@ -10,17 +10,22 @@ public class subarray {
         System.out.println(Arrays.toString(arr));
         System.out.println();
 
-        int limit = 0;
+        // int limit = 0;
         // int k = 0;
+        for (int z = 0; z < arr.length; z++) {
+            int limit = 0;
+            // int ilk = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            int k = 0;
-            for (int j = 0; j < arr.length - limit; j++) {
-                System.out.print(arr[j] + " ");
+            for (int i = 0; i < arr.length; i++) {
+                int k = 0;
 
+                for (int j = 0; j < arr.length - limit; j++) {
+                    System.out.print(arr[j] + " ");
+
+                }
+                limit++;
+                System.out.println("\n");
             }
-            limit++;
-            System.out.println("\n");
         }
     }
 }
